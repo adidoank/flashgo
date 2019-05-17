ec27607ee0857b9294a69621c75e4cdc6fc4b113
Dika91 <50719985+Dika91@users.noreply.github.com>
2019-05-17 13:27:01 +0700

    Update config.php

--- config.php
+++ config.php
@@ -3,21 +3,21 @@
 
 
 // user_name boleh kosong //
-$user_name="";
+$user_name="Rahma Andika Mangunsong";
 
 //user_gaid wajib isi
-$user_gaid= "xxxxxxxxxxxxxxxxxx";
+$user_gaid= "79e001e1-b714-4148-8857-66d6edef88d2";
 //user_token wajib di isi //
-$user_token="xxxxxxxxxxxxxxxxxx";
+$user_token="Abz4GYFDrGMXtfHA";
 
 //user_id wajib di isi //
-$user_id="Facebook_xxxxxxxxxxxxxxxxxx";
+$user_id="Facebook_738725069855858";
 
 // invite_code wajib di isi //
-$invite_code="xxxxxxxxxxxxxxxxxx";
+$invite_code="F3NlfU5f86";
 
 // Bearer wajib di isi //
-$Bearer="eyxxxxxxxxxxxxxxxxxx";
+$Bearer="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va183Mzg3MjUwNjk4NTU4NTgiLCJleHAiOiIxNTYwNjY1MjE4NTk3In0._UzOeA7L4ll8jc8mi7h2Vl-uGXHKs9Q8tAKFvshzxDs";
 
 
 
